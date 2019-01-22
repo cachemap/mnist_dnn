7b7b28b109c1a1b7a9ed4aef3d24638cf52744a4
Dillon <dillon.notz@gmail.com>
2019-01-21 23:16:50 -0500

    Create first Keras-based model file

---
+++ src/keras_v1/model.py
@@ -0,0 +1,6 @@
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
